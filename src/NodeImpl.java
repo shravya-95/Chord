f1681bd0621030728bcc7c5efd216066a088ba3e
Shravya Gade <gk.shravya@gmail.com>
2021-04-18 13:48:17 -0600

    setup

---
+++ src/NodeImpl.java
@@ -0,0 +1,17 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface NodeImpl extends Remote {
+
+    public String  findSuccessor (int key, boolean traceFlag) throws RemoteException;
+    public String  findPredecessor (int key) throws RemoteException;
+    public String  closestPrecedingFinger (int key) throws RemoteException;
+    public String  successor () throws RemoteException;
+    public String  predecessor  () throws RemoteException;
+    public boolean join (String nodeURL) throws RemoteException;
+    public boolean joinFinished (String nodeURL) throws RemoteException;
+    public boolean insert (String word, String definition) throws RemoteException;
+    public String  lookup (String word) throws RemoteException;
+    public String  printFingerTable() throws RemoteException;
+    public String  printDictionary() throws RemoteException;
+}
