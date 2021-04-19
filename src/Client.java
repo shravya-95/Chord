9b0d6c00ddc4cd77af1cb6002271c1859c379bce
aiBoss <bmouryakaranreddy@gmail.com>
2021-04-18 20:53:46 -0500

    Client

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,31 @@
-public class Client {
+import java.util.Scanner;
+import java.rmi.RMISecurityManager;
+import java.rmi.Naming;
+import java.util.*;
+import java.io.*;
+import java.rmi.RemoteException;
+
+public class Client{
+    public static void main(String[] args) throws Exception{
+        if ( args.length != 2 ) {
+            throw new RuntimeException( "Syntax: java Client someChordNodeURL \n URL should be sent in the form alpha.umn.edu:1099 where 1099 is the port" );
+        }
+        System.setSecurityManager (new SecurityManager ());
+        Node node = (Node) Naming.lookup ("//" + args[0] +"/node00");
+
+        System.out.println("Enter 1 to lookup, 2 to exit");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your choice: ");
+        int choice = in.nextInt();
+        while(choice!=2){
+            System.out.println("Enter your word: ");
+            String word = in.next();//Assuming no space in word
+            int wordHash = FNV1aHash.hash32(word);//assuming this returns correct without
+            Node successor = node.findSuccessor(wordHash,false);
+            String meaning = successor.lookup(word);
+            System.out.println("Result: "+meaning);
+            System.out.print("Enter your choice: ");
+            choice = in.nextInt();
+        }
+    }
 }
